c533bb275cf5dabf161b0117a1901ad78f6eda02
Carlos Olivera <colivera@hg.com.uy>
2025-04-15 16:11:40 -0300

    commit empty project ready to start working on it

---
+++ src/main/java/com/carloso/ai/openaitoolcallback/AiOpenaiToolCallbackApplication.java
@@ -0,0 +1,13 @@
+package com.carloso.ai.openaitoolcallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiOpenaiToolCallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiOpenaiToolCallbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carloso/ai/openaitoolcallback/AiOpenaiToolCallbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloso.ai.openaitoolcallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiOpenaiToolCallbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
